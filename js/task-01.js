26323d129a3c39e075fb86dd885207c6edf2e46f
Lyudmila Maximenko <109598383+LyudmilaMax@users.noreply.github.com>
2022-12-05 12:48:02 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,18 @@
 
-const totalCategories = document.querySelectorAll('#categories li.item')
-console.log(`Number of categories: ${totalCategories.length}`);
+const categoriesList = document.querySelectorAll('.item');
+console.log(`Number of categories: ${categoriesList.length}`)
 
-const categoriesArray = [...totalCategories]
-  .map(categories => `Category: ${categories.children[0].textContent}
-Elements: ${categories.children[1].children.length}`
-  )
-  .join("\n");
-console.log(categoriesArray);
+categoriesList.forEach(item => {
+    console.log(`Category: ${item.firstElementChild.textContent}`);
+    console.log(`Elements: ${item.lastElementChild.children.length}`)
+});
+
+// const totalCategories = document.querySelectorAll('#categories li.item')
+// console.log(`Number of categories: ${totalCategories.length}`);
+
+// const categoriesArray = [...totalCategories]
+//   .map(categories => `Category: ${categories.children[0].textContent}
+// Elements: ${categories.children[1].children.length}`
+//   )
+//   .join("\n");
+// console.log(categoriesArray);
